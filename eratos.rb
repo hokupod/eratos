d8e2ccfe2856367a5b624c7e84fe74ff09fa7cb1
hokupod <fireface800rme@gmail.com>
2018-06-09 23:14:37 +0900

    Initialize test.

---
+++ eratos.rb
@@ -0,0 +1,5 @@
+class Eratos
+  def self.sift
+    '2, 3, 5, 7, 11, 13, 17, 19, 23, 29'
+  end
+end
---
+++ spec/eratos_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require './eratos'
+
+describe Eratos do
+  describe '#sift' do
+    subject { Eratos.sift }
+    let(:base_num) { 30 }
+    it { is_expected.to eq '2, 3, 5, 7, 11, 13, 17, 19, 23, 29' }
+  end
+end
