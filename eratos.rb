1806e3db50b9b9a46e6d7c94f3dce955fb81b694
hokupod <fireface800rme@gmail.com>
2018-06-19 19:59:12 +0900

    Implementation of sift method.

--- eratos.rb
+++ eratos.rb
@@ -1,5 +1,33 @@
 class Eratos
-  def self.sift(base_num)
-    '2, 3, 5, 7, 11, 13, 17, 19, 23, 29'
+  attr_reader :base_num, :range, :threshold
+  attr_accessor :primes
+
+  def initialize(base_num)
+    @base_num = base_num
+    @range = (2..base_num).to_a
+    @primes = []
+    @threshold = Math.sqrt(base_num)
+  end
+
+  def sift
+    dup_range = range.dup
+    self.primes = []
+    sieve(dup_range)
+  end
+
+  private
+
+  def sieve(range)
+    primes << first = range.shift
+    range.reject! { |num| (num % first).zero? }
+    return sieve(range) unless first > threshold
+    primes.concat(range)
   end
 end
+
+if __FILE__ == $0
+  input = ARGV.first.to_i
+  eratos = Eratos.new input
+  puts eratos.sift.join(', ')
+end
+
--- spec/eratos_spec.rb
+++ spec/eratos_spec.rb
@@ -3,7 +3,8 @@ require './eratos'
 
 describe Eratos do
   describe '#sift' do
-    subject { Eratos.sift base_num }
+    subject { eratos.sift.join(', ') }
+    let(:eratos) { Eratos.new base_num }
     let(:base_num) { 30 }
     it { is_expected.to eq '2, 3, 5, 7, 11, 13, 17, 19, 23, 29' }
   end
